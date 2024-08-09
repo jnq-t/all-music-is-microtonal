f8d97f28f9bc810f9abc034cc3febd801de5fdd9
pluto bell <pluto.belli@gmail.com>
2024-08-08 17:15:39 -0700

    create index.js

---
+++ mongodb-mongoose/index.js
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose'
+
+mongoose.connect("mongodb+srv://jnqt:js03JmHarCj95VAq@all-music-is-microtonal.ullxm.mongodb.net/?retryWrites=true&w=majority&appName=all-music-is-microtonal")
