72c184cf04d7f60010d9148687a2f2f27dba049a
Jesse Quebbeman-Turley <jesse@Jesses-MBP.lan>
2024-08-13 15:54:46 -0700

    refresh style structure

---
+++ app/models/Scale.js
@@ -0,0 +1,41 @@
+// import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+
+const scaleSchema = new Schema({
+	name: {
+		type: String,
+		unique: true
+	},
+	author: {
+		type: String,
+		default: ""
+	},
+	isPreset: {
+		type: Boolean,
+		default: false,
+		required: true
+	},
+	scaleDegrees: {
+		type: Array,
+		default: []
+	},
+	sustainMode: {
+		type: Boolean,
+		default: false,
+		required: true
+	},
+	startingFreq: {
+		type: Number,
+		default: 240,
+		required: true
+	},
+	periodRatio: {
+		type: Object,
+		default: { numerator: 2, denominator: 1 },
+		required: true
+	},
+});
+
+module.exports = Scale = mongoose.model('scale', scaleSchema);
