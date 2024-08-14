9e10d87bda33c3d5a4c39a1f8c739fc34b17f523
pluto bell <pluto.belli@gmail.com>
2024-08-13 17:57:49 -0700

    import scale route to server

--- server/routes/scale.js
+++ server/routes/scale.js
@@ -1,54 +1,20 @@
 // routes/api/scale.js
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
 // Load Scale Model
 const Scale = require('../../app/models/Scale')
 
-// Get a list of 50 scales
-router.get("/", async (req, res) => {
-    let collection = await db.collection("scales");
-    let results = await collection.find({})
-      .limit(50)
-      .toArray();
-    res.send(results).status(200);
-  });
 
-// Get a single post
-router.get("/:id", async (req, res) => {
-    let collection = await db.collection("scales");
-    let query = {_id: ObjectId(req.params.id)};
-    let result = await collection.findOne(query);
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
-  });
+router.get('/test', (req, res) => res.send('book route testing!'))
 
-// Create new scale
-router.post("/", async (req, res) => {
-    let collection = await db.collection("scales");
-    let newDocument = req.body;
-    newDocument.date = new Date();
-    let result = await collection.insertOne(newDocument);
-    res.send(result).status(204);
-  });
 
-// Update scale
-router.patch("/scale/:id", async (req, res) => {
-    const query = { _id: ObjectId(req.params.id) };
-    const updates = {
-      $push: { scales: req.body }
-    };
-    let collection = await db.collection("scales");
-    let result = await collection.updateOne(query, updates);
-    res.send(result).status(200);
-  });
+  router.get('/', (req, res) => {
+    Scale.find()
+        .then(books => res.json(books))
+        .catch(err => res.status(404).json({noScalesFound: 'No scales found.'}))
+});
 
-  // Delete scale
-router.delete("/:id", async (req, res) => {
-    const query = { _id: ObjectId(req.params.id) };
-    const collection = db.collection("scales");
-    let result = await collection.deleteOne(query);
-    res.send(result).status(200);
-  });
 
   module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const connectDB = require('./db');
 //Express Node Intro: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction
 const express = require('express');
+const scaleRoute = require("./routes/scale");
 const app = express();
 
 const { port } = require('./config');
@@ -12,3 +13,5 @@ connectDB().catch(console.dir);
 // Connect to localhost port
 app.get('/', (req, res) => res.send('Hello world!'));
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use("/scales", scaleRoute);
