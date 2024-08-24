6be68940280a06bc180d95376d945f5f088fcf8b
Jesse Quebbeman-Turley <jesse.quebbeman@mx.com>
2024-08-23 19:02:41 -0700

    finalllly working scale degrees with modifiers

--- app/TEMP_classes_workspace.js
+++ app/TEMP_classes_workspace.js
@@ -21,11 +21,12 @@ class Scale {
      * **/
     scaleDegrees() {
         const indexedModifiers = this.#indexedModifiersArray()
-        let scaleDegrees = [new ScaleDegree(this.startingFreq, indexedModifiers[0], this.startingFreq)];
+        let scaleDegrees = [new ScaleDegree(this.startingFreq, (indexedModifiers[0] || new ScaleDegreeModifier()), this.startingFreq)];
         while (scaleDegrees.length < this.length) {
             const previousScaleDegree = scaleDegrees.slice(-1)[0]
-            const nextFrequency = previousScaleDegree.inputFrequency * this.#stepSizeMultiplier()
-            scaleDegrees.push(new ScaleDegree(nextFrequency, indexedModifiers[scaleDegrees.length], this.startingFreq));
+            const nextFrequency = (previousScaleDegree.inputFrequency) * this.#stepSizeMultiplier()
+            const modifier = indexedModifiers[scaleDegrees.length] || new ScaleDegreeModifier()
+            scaleDegrees.push(new ScaleDegree(nextFrequency, modifier, this.startingFreq));
         };
         return scaleDegrees;
     };
@@ -62,37 +63,20 @@ class ScaleDegree {
         this.inputFrequency = inputFrequency
         this.modifier = modifier
         this.startingFreq = startingFreq
-        // i know js ppl don't like doing this in the constructor but my ruby brain loves it haha
         this.frequency = this.#frequency()
     }
-
     #frequency() {
-        if (typeof this.modifier === "undefined") {
-            return this.inputFrequency
-        }
-
+        let frequency = this.inputFrequency;
         const ratio = this.#ratio();
-        const detuneByCents = this.modifier.detuneByCents;
-        let frequency = this.inputFrequency
-
-        if (typeof ratio != "undefined") {
-            frequency = this.startingFreq * ratio
-        }
-
-        if (typeof detuneByCents != "undefined"){
-        // TODO this is old math, double check it
-            frequency *= Math.pow(2, detuneByCents/1200);
-        }
+        frequency *= ratio;
+        frequency *= Math.pow(2, this.modifier.detuneByCents/1200);
         return frequency;
     };
 
     #ratio(){
-        const numerator = this.modifier.ratioNumerator
-        const denominator = this.modifier.ratioDenominator
-
-        if (typeof numerator != "undefined" && typeof denominator != "undefined") {
-            return numerator / denominator;
-        }
+        if (this.modifier.ratioDenominator == 0) return 1;
+        const ratio = this.modifier.ratioNumerator / this.modifier.ratioDenominator
+        return (ratio || 1);
     };
 };
 
@@ -102,46 +86,33 @@ class Keyboard {
         this.cutoffFrequency = 20_000;
     }
 
-    // #buildKeyboardFrequencies() {
-    //     const scaleDegrees = this.scale.scaleDegrees();
-    //     let scaffold = [];
-    //     let i = 0;
-    //     while (true){
-    //         let freq = scaleDegrees[i % this.scale.length]
-    //     }
-    // }
+    buildKeyboardFrequencies() {
+        const scaleDegrees = this.scale.scaleDegrees();
+        let scaffold = [];
+        let i = 0;
+        while (true){
+            // calculation
+            const currentPeriod = Math.floor(i/this.scale.length)
+            const periodMultiplier = currentPeriod * this.scale.period || 1; // making sure we don't zero out the first row
+            const baseFrequency = scaleDegrees[(i % this.scale.length)].frequency
+            const frequency = baseFrequency * periodMultiplier
+
+            // return clause
+            if (frequency >= this.cutoffFrequency) break;
+
+            // assignment
+            scaffold[i] = frequency
+            i++;
+        }
+        return scaffold;
+    }
 
 
     // private functions
-    #buildKeyboardFrequencies() {
-        const scaleDegrees = this.scale.scaleDegrees(); // initializes 1st period of scaleDegrees objects
-        let scaffold = scaleDegrees.map(scaleDegree => scaleDegree.frequency); // creates base "layer" via scaleDegrees freq.'s
-
-        for (let scaleIndex = 0; scaleIndex < this.scale.length; scaleIndex++) { 
-            let keyboardIndex = 1;  // sets fresh cursor for each scaleDegree
-
-            while (true) { // inner loop iterates once for each "octave"
-                
-                const position = (keyboardIndex * this.scale.length) + scaleIndex; // uses keyboard & scale index to locate the correct position in 2d array
-               
-                // calculates frequency by referencing the last frequency we added to the scaffold
-                const prevPosition = position - this.scale.length; 
-                const prevFrequency = scaffold[prevPosition];
-
-                let frequency = this.scale.period * prevFrequency; // finds new frequency by multiplying the previous frequency by the period (octave interval)
-
-                if (frequency >= this.cutoffFrequency) break;
-                
-                scaffold[position] = frequency; // pushes frequency into array
-                keyboardIndex++;
-            }
-        };
-        return scaffold;
-    };
 
     keys() {
-        const keysFreqArr = this.#buildKeyboardFrequencies();
-        console.log(keysFreqArr);
+        // const keysFreqArr = this.#buildKeyboardFrequencies();
+        // console.log(keysFreqArr);
         // keysFreqArr.map(); //TODO
     };
 };
@@ -169,8 +140,8 @@ class Key {
 };
 
 //todo: short description on class; 
-class scaleDegreeModifier {
-    constructor(scaleDegreePosition = 0, ratioNumerator = 2, ratioDenominator = 1, detuneByCents = 0) {
+class ScaleDegreeModifier {
+    constructor(scaleDegreePosition = 0, ratioNumerator = 0, ratioDenominator = 0, detuneByCents = 0) {
         this.scaleDegreePosition = scaleDegreePosition
         this.ratioNumerator = ratioNumerator
         this.ratioDenominator = ratioDenominator
@@ -179,16 +150,16 @@ class scaleDegreeModifier {
 };
 
 function getModifiersForScale(scaleId) {
-    const modifier0 = new scaleDegreeModifier(5,5,4,0);
-    const modifier1 = new scaleDegreeModifier(0,0,0,20);
-    const modifier2 = new  scaleDegreeModifier(9,7,4,0);
+    const modifier0 = new ScaleDegreeModifier(5,5,4,0);
+    const modifier1 = new ScaleDegreeModifier(0,0,0,20);
+    const modifier2 = new ScaleDegreeModifier(9,7,4,0);
     const mockedResponse = {
         "ok": 1,
-        "documents": [
-            modifier0,
-            modifier1,
-            modifier2
-        ]
+        "documents": []
+        //     modifier0,
+        //     modifier1,
+        //     modifier2
+        // ]
     };
     return mockedResponse;
 }
@@ -215,8 +186,9 @@ function activateTone() {
 
 const newScale = new Scale();
 newScale.name = 'plups scale'
-// const keyboardWithScaleInjected = new Keyboard(newScale);
+const keyboardWithScaleInjected = new Keyboard(newScale);
 // console.log('Keyboard w/ Scale Injected: ', keyboardWithScaleInjected)
 // keyboardWithScaleInjected.keys()
 // const keyboardTest = new Keyboard(newScale)
-console.log(newScale.scaleDegrees())
+// console.log(newScale.scaleDegrees())
+console.log(keyboardWithScaleInjected.buildKeyboardFrequencies())
