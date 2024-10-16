54a034c248450f24bbc413bd80484fd0a18a4067
pluto <99231462+pluto-bell@users.noreply.github.com>
2024-10-16 15:21:51 -0700

    clean up

--- app/use_cases/generate_scale/createKeys.js
+++ app/use_cases/generate_scale/createKeys.js
@@ -3,41 +3,38 @@ import createEle from "../../utilities/createBasicDomElement.js"
 import roundFreq from "../../utilities/roundFreq.js"
 import KeyModifiers from "./createKeyModifiers.js"
 
-//todo: refactor code
+/**
+ * @method createKeyElement creates keys for keyboard; includes modifiers and button for playing frequency
+ * @param {Object} key object {}
+ * @param {Index} index of key
+ * @param {Object} currentKeyboard of keyboard class
+ * @param {Function} container <div/> element to append each key to keysContainer
+ */
+export default function createKeyElement (key, index, container, lengthOfScale, currentKeyboard) {
+  
+  const keyContainer = createEle('div', `${ index < lengthOfScale ? 'key-container-show' : 'key-container-hidden'}`) // initial logic for which keys to show/hide
+        keyContainer.id = `key-container${index+1}`
+  const keyModifiersContainer = createEle('div', 'scale-degree-modifiers-container')
+  const btn = Object.assign(document.createElement('button'), 
+      { className: 'keyboard-key' },
+      { id: `keyboard-key${index+1}` }, 
+      { name: `${key.frequency}` }, 
+      { innerHTML: `${roundFreq(key.frequency, 3)}` }, // todo: change to scale position (e.g. scale degree "8") + rounded freq.
+      { ariaLabel: 'Button to play frequency' }
+  );
+      btn.setAttribute('index', index)
+  
+  // append items 
+  //* creates sustain modifier for each key */
+  keyModifiersContainer.appendChild( KeyModifiers.createSustainModifier(index, lengthOfScale, currentKeyboard))
 
-  /**
-   * @method createKeyElement creates keys for keyboard; includes modifiers and button for playing frequency
-   * @param {Object} key object {}
-   * @param {Index} index of key
-   * @param {Object} currentKeyboard of keyboard class
-   * @param {Function} container <div/> element to append each key to keysContainer
-   */
-  export default function createKeyElement (key, index, container, lengthOfScale, currentKeyboard) {
-    
-    const keyContainer = createEle('div', `${ index < lengthOfScale ? 'key-container-show' : 'key-container-hidden'}`) // initial logic for which keys to show/hide
-          keyContainer.id = `key-container${index+1}`
-
-    const keyModifiersContainer = createEle('div', 'scale-degree-modifiers-container')
-          
-    const btn = Object.assign(document.createElement('button'), 
-        { className: 'keyboard-key' },
-        { id: `keyboard-key${index+1}` }, 
-        { name: `${key.frequency}` }, 
-        { innerHTML: `${roundFreq(key.frequency, 3)}` }, // todo: change to scale position (e.g. scale degree "8") + rounded freq.
-        { ariaLabel: 'Button to play frequency' }
-        // { setAttribute: `key-position-${position}` } // todo: add scale position; should it be an attribute or additional class? google use case
-    );
-        btn.setAttribute('index', index)
-    
-    keyModifiersContainer.appendChild( KeyModifiers.createSustainModifier(index, lengthOfScale, currentKeyboard)) // append sustain modifier
-     if (index < lengthOfScale) { // appends detune and ratio modifier to first octave only 
-      keyModifiersContainer.appendChild(KeyModifiers.createDetuneModifier(index, lengthOfScale, currentKeyboard))
-      keyModifiersContainer.appendChild(KeyModifiers.createRatioModifier(index, lengthOfScale, currentKeyboard))
-    }
-
-    keyContainer.appendChild(btn); // appends keyBtn to parent <div/> => keyContainer
-    keyContainer.appendChild(keyModifiersContainer) // appends key modifiers to parent <div/> => keyContainer
-
-    //appends each key to parent <div/> => keysContainer
-    container.appendChild(keyContainer);
+  //* creates detune and ratio modifier for first octave only */
+  if (index < lengthOfScale) {
+    keyModifiersContainer.appendChild(KeyModifiers.createDetuneModifier(index, lengthOfScale, currentKeyboard))
+    keyModifiersContainer.appendChild(KeyModifiers.createRatioModifier(index, lengthOfScale, currentKeyboard))
   }
+
+  keyContainer.appendChild(btn); 
+  keyContainer.appendChild(keyModifiersContainer);
+  container.appendChild(keyContainer);
+}
